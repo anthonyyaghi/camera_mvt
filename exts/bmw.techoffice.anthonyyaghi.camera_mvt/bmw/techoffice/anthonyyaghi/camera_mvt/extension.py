cf6cc4ca121030ad336be8a0b103f29508cb3df0
anthonyyaghi <anthonyyaghi@outlook.com>
2022-07-17 16:58:23 +0200

    Skeleton for circular path and bounding region

--- exts/bmw.techoffice.anthonyyaghi.camera_mvt/bmw/techoffice/anthonyyaghi/camera_mvt/extension.py
+++ exts/bmw.techoffice.anthonyyaghi.camera_mvt/bmw/techoffice/anthonyyaghi/camera_mvt/extension.py
@@ -1,120 +1,101 @@
 import omni.ext
 import omni.ui as ui
+import omni.kit.commands
 import omni.replicator.core as rep
 
+from pxr import Usd, UsdGeom, Gf, Sdf
 
-# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
-# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
-# on_shutdown() is called.
+from .utils import *
+
+
+fields = {"string": ui.StringField, "int": ui.IntDrag, "float": ui.FloatDrag, "multi_float": ui.MultiFloatField}
+
 class MyExtension(omni.ext.IExt):
-    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
-    # this extension is located on filesystem.
     def on_startup(self, ext_id):
         print("[bmw.techoffice.anthonyyaghi.camera_mvt] startup")
-        self.stage = omni.usd.get_context().get_stage()
-        self.prims = {"camera": None, "lookat": None, "path": None}
 
+        # Register custom randomizers
+        from .custom_nodes import CreateCircularPathRandomizer
+        CreateCircularPathRandomizer()
+
+        # Class wide variables
+        self.stage = omni.usd.get_context().get_stage()
+        self.prims = {}
+
+        # UI
         self._window = ui.Window("Image collector", width=300, height=300)
         with self._window.frame:
-            def drop_accept(url):
-                """Called to check the drag data is acceptable."""
-                return True
-
-            def drop(event, widget, prim, error_label, validator=None, ):
-                """Called when dropping the data."""
-                widget.model.set_value(event.mime_data)
-                self.prims[prim] = self.stage.GetPrimAtPath(widget.model.as_string)
+            def check(m, error_label, prim, validator=None, ):
+                self.prims[prim] = self.stage.GetPrimAtPath(m.get_value_as_string())
                 if validator is not None and not validator(self.prims[prim]):
                     error_label.text = "Invalid Object!"
                 else:
                     error_label.text = ""
 
-            def drop_area(label, prim, validator=None):
-                """A drop area that shows object path when droped"""
-                with ui.HStack(height=15):
-                    ui.Label(label)
-                    field = ui.StringField()
-                    error_label = ui.Label("", style={"color": "red"})
-
-                field.enabled = False
-                field.set_accept_drop_fn(drop_accept)
-                field.set_drop_fn(lambda event, widget=field: drop(event, widget, prim, error_label,
-                                                                   validator=validator))
+            def val_labeled_field(label, type, validator=None, prim=None):
+                with ui.VStack():
+                    with ui.HStack(height=15):
+                        ui.Label(label)
+                        field = fields[type]()
+                    error_label = ui.Label("", style={"color": "red"}, height=15)
+                
+                if validator is not None and prim is not None:
+                    field.model.add_value_changed_fn(lambda m, label=error_label: check(m, label, 
+                                                                                        validator=validator, prim=prim))
+                return field
+            
+            def multi_labeled_field(label, type, *args):
+                with ui.VStack():
+                    ui.Label(label, height=15)
+                    field = fields[type](*args, height=15)
                 return field
 
-            with ui.VStack(style={"margin": 1}):
-                drop_area("Camera", "camera", validator=self.camera_validator)
-                drop_area("Look At", "lookat", validator=self.prim_validator)
-                drop_area("Camera Path", "path", validator=self.path_validator)
-                ui.Button("Create", height=15, clicked_fn=self.get_path_points)
+            with ui.ScrollingFrame():
+                with ui.VStack(style={"margin": 1}):
+                    # Circular Path UI
+                    with ui.CollapsableFrame(title="Circular Path"):
+                        with ui.VStack(style={"margin": 1}):
+                            val_labeled_field("Look At", "string", validator=prim_validator, prim="lookat")
+                            self.distance_field = val_labeled_field("Distance", "float")
+                            self.elevation_field = val_labeled_field("Elevation", "float")
+                            self.circular_frames_field = val_labeled_field("Number of frames", "int")
+                            ui.Button("Create", height=15, clicked_fn=self.create_circular_path)
+                    # Bounding Region UI
+                    with ui.CollapsableFrame(title="Bounding Region"):
+                        with ui.VStack(style={"margin": 1}):
+                            val_labeled_field("Look At", "string", validator=prim_validator, prim="lookat")
+                            self.from_field = multi_labeled_field("From", "multi_float", 0.0, 0.0, 0.0)
+                            self.to_field = multi_labeled_field("To", "multi_float", 0.0, 0.0, 0.0)
+                            self.region_frames_field = val_labeled_field("Number of frames", "int")
+                            ui.Button("Create", height=15, clicked_fn=self.create_bounding_region)
+                    # Reset Button
+                    ui.Button("Reset", height=15, clicked_fn=self.delete_replicator_prim)
+                
 
-    def on_shutdown(self):
-        print("[bmw.techoffice.anthonyyaghi.camera_mvt] shutdown")
+    def create_circular_path(self):
+        look_at = str(self.prims["lookat"].GetPath())
+        distance = self.distance_field.model.get_value_as_int()
+        elevation = self.elevation_field.model.get_value_as_int()
+        frames = self.circular_frames_field.model.get_value_as_int()
+        angle = 360.0/frames
 
-    def camera_validator(self, prim):
+        with rep.trigger.on_frame(num_frames=frames):
+            randomizer = rep.randomizer.compute_camera_pos(distance, elevation, angle)
+            generate_graph(randomizer, [look_at])
+    
+    def create_bounding_region(self):
+        look_at = str(self.prims["lookat"].GetPath())
+        from_point = self.from_field
+        to_point = self.to_field
+        frames = self.region_frames_field.model.get_value_as_int()
+
+        with rep.trigger.on_frame(num_frames=frames):
+            randomizer = rep.distribution.uniform(from_point, to_point)
+            generate_graph(randomizer, [look_at])
+        
+    
+    def delete_replicator_prim(self, ):
         try:
-            return self.prim_validator(prim) and prim.GetTypeName() == "Camera"
-        except RuntimeError:
-            return False
-
-    def path_validator(self, prim):
-        try:
-            return self.prim_validator(prim) and prim.GetTypeName() == "BasisCurves"
-        except RuntimeError:
-            return False
-
-    def prim_validator(self, prim):
-        try:
-            return prim.IsValid()
-        except RuntimeError:
-            return False
-
-    def get_path_points(self):
-        points = [tuple(point) for point in self.prims["path"].GetAttribute("points").Get()]
-        # MyExtension.define_path_lookat_obj(self.prims["camera"], points, [self.prims["lookat"]])
-        with rep.new_layer():
-            camera = rep.create.camera(position=(0, 0, 1000))
-            render_product = rep.create.render_product(camera, (1024, 1024))
-            # torus = rep.create.torus(semantics=[('class', 'torus')] , position=(0, -200 , 100))
-            # sphere = rep.create.sphere(semantics=[('class', 'sphere')], position=(0, 100, 100))
-            # cube = rep.create.cube(semantics=[('class', 'cube')],  position=(100, -200 , 100) )
-
-            with rep.trigger.on_frame(num_frames=10):
-                with rep.create.group([camera]):
-                    rep.modify.pose(
-                        position=rep.distribution.uniform((-100, -100, -100), (200, 200, 200)),
-                        look_at=[str(self.prims["lookat"].GetPath())])
-
-            # Initialize and attach writer
-            # writer = rep.WriterRegistry.get("BasicWriter")
-
-            # writer.initialize( output_dir="C:\Users\antho\Downloads\output", rgb=True,   bounding_box_2d_tight=True)
-
-            # writer.attach([render_product])
-
-            rep.orchestrator.preview()
-
-
-    @staticmethod
-    def define_path_lookat_obj(name_path, coord, lookat):
-        # Create DR transform component
-        result, prim = omni.kit.commands.execute(
-            "CreateTransformComponentCommand",
-            prim_paths=[name_path],
-            target_points=coord,
-            target_paths=lookat,
-            enable_sequential_behavior=True
-        )
-        return prim
-
-    @staticmethod
-    def define_path_lookat_points(name_path, coord, lookat):
-        # Create DR transform component
-        result, prim = omni.kit.commands.execute(
-            "CreateTransformComponentCommand",
-            prim_paths=[name_path],
-            target_points=coord,
-            lookat_target_points=lookat,
-            enable_sequential_behavior=True
-        )
-        return prim
+            omni.kit.commands.execute('DeletePrims', paths=['/Replicator'])
+        except:
+            pass
